232224479736d99f49dd498a1862b6b4e069b47b
Xa4vier <Xavier_vinken@hotmail.com>
2018-10-24 12:43:44 +0200

    alle dataset + functie toegevoegd

--- mathplottest.py
+++ mathplottest.py
@@ -4,15 +4,22 @@ import matplotlib.pyplot as plt
 
 from preprocessing import get_dataset_list, get_X, get_Y
 
-dataset = get_dataset_list("datasets/dataset_koppel.csv")
-namen = dataset[0]
-del dataset[0]
+def plot(name):
+    
+    dataset = get_dataset_list(f"datasets/dataset_{name}.csv")
+    del dataset[0]
+    
+    x = get_X(0, 1, dataset)
+    y = get_Y(1, dataset)
 
-x = get_X(0, 1, dataset)
-y = get_Y(1, dataset)
 
-plt.plot(x, y)
-plt.hlines(2500, 0, 23, colors='red', linestyles='solid')
-plt.xticks(range(0, 25, 2))
-plt.yticks(range(0, 4001, 500))
-plt.show()
+    plt.plot(x, y)
+    plt.hlines(2500, 0, 23, colors='red', linestyles='solid')
+    plt.xticks(range(0, 25, 2))
+    plt.yticks(range(0, 4001, 500))
+    plt.show()
+    
+names = ["bejaard_koppel", "gezin_kinderen", "student", "koppel"]
+
+for name in names:
+    plot(name)
