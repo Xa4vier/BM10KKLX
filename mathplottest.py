2ceda6f3e0fb2a424e538eabbb07470df35ba7b6
Xa4vier <Xavier_vinken@hotmail.com>
2018-11-02 14:14:44 +0100

    .

--- mathplottest.py
+++ mathplottest.py
@@ -47,66 +47,66 @@ def index_peak(y):
             temp = []
 
 names = ["bejaard_koppel", "gezin_kinderen", "student", "koppel"]
-#name = names[0]
-for name in names:
+name = names[0]
+#for name in names:
 	
-    # get the dataset
-    dataset = get_dataset_list(f"datasets/dataset_{name}.csv")
-    del dataset[0]
+# get the dataset
+dataset = get_dataset_list(f"datasets/dataset_{name}.csv")
+del dataset[0]
+
+# get the X and Y and plot the dataset 
+x = get_X(0, 1, dataset) # get X
+y = get_Y(1, dataset) # get Y
+plot(x, y, range(0, 25, 2), range(0, 6001, 500), 2500) # plot datapoints
+dataset = [[int(i[0]), int(i[1])] for i in dataset] # make a list with 2 int in a list
+
+# Compute the area using the composite Simpson's rule.
+y = [i[1] for i in dataset] # get all the Y 
+totalArea = simps(y, dx=1) # the total area
+
+# get the dataset but only above the peak line
+datasetAbove = calculate_above_area(dataset, 2500)    
+y = [i[1] for i in datasetAbove] # get only the y variable
+
+aboveArea = simps(y, dx=1) # calculate the above area with the simpson's rule
+underArea = totalArea - aboveArea # subtract the above area of the total area to get the under area
+
+# plot the above area
+#plot(get_X(0, 1, datasetAbove), get_Y(1, datasetAbove), range(0, 25, 2), range(0, 6001, 500))
+
+
+print(f'Gebruiker: {name}\nTotal: {totalArea}\nAbove: {aboveArea}\nUnder: {underArea}')
+
+# get all peaks 
+peaks = get_peaks(y)
+for i in range(len(peaks)):
+    print(f'peak {i}: {simps(peaks[i], dx=1)}')
+
+topFlatteningRate = .025 # this is the rate where the peaks will be cut off
+curveRate = .1 # dit is het percentage waarmee het eerst volgende datapunt word verminderd
+maxY = 2500 # this is the max where the graph has to be under
+
+for peak in peaks: # loop trough al peaks
     
-    # get the X and Y and plot the dataset 
-    x = get_X(0, 1, dataset) # get X
-    y = get_Y(1, dataset) # get Y
-    plot(x, y, range(0, 25, 2), range(0, 6001, 500), 2500) # plot datapoints
-    dataset = [[int(i[0]), int(i[1])] for i in dataset] # make a list with 2 int in a list
+    indexes = [peak.index(i) for i in peak if i != 0] # get all the indexes of the peak
+    topFlattening= maxY * topFlatteningRate # calculate the flattening rate
+    for i in indexes: # set the datapoints of the peak to the new datapoint minus the flattening rate
+        dataset[i][1] = maxY - topFlattening
     
-    # Compute the area using the composite Simpson's rule.
-    y = [i[1] for i in dataset] # get all the Y 
-    totalArea = simps(y, dx=1) # the total area
+    sumPeak = sum(peak) + topFlattening * len(peaks) # calculate the total peak
     
-    # get the dataset but only above the peak line
-    datasetAbove = calculate_above_area(dataset, 2500)    
-    y = [i[1] for i in datasetAbove] # get only the y variable
-    
-    aboveArea = simps(y, dx=1) # calculate the above area with the simpson's rule
-    underArea = totalArea - aboveArea # subtract the above area of the total area to get the under area
-    
-    # plot the above area
-    plot(get_X(0, 1, datasetAbove), get_Y(1, datasetAbove), range(0, 25, 2), range(0, 6001, 500))
-    
-      
-    print(f'Gebruiker: {name}\nTotal: {totalArea}\nAbove: {aboveArea}\nUnder: {underArea}')
-    
-    # get all peaks 
-    peaks = get_peaks(y)
-    for i in range(len(peaks)):
-        print(f'peak {i}: {simps(peaks[i], dx=1)}')
-    
-    topCreamingRate = .025 # this is the rate where the peaks will be cut off
-    curveRate = .1 # dit is het percentage waarmee het eerst volgende datapunt word verminderd
-    maxY = 2500 # this is the max where the graph has to be under
-    
-    for peak in peaks: # loop trough al peaks
-        
-        indexes = [peak.index(i) for i in peak if i != 0] # get all the indexes of the peak
-        topCreaming = maxY * topCreamingRate # calculate the creaming rate
-        for i in indexes: # set the datapoints of the peak to the new datapoint minus the creaming rate
-            dataset[i][1] = maxY - topCreaming
-        
-        sumPeak = sum(peak) + topCreaming * len(peaks) # calculate the total peak
-        
-        previousIndex = indexes[0] - 1 # get the index before the begining of the peak
-        while sumPeak > 0 and previousIndex != 0: # loops till sumPeak is 0
-            maxPrevious = dataset[previousIndex + 1][1] * (1 - curveRate)
-            diffrence = maxPrevious - dataset[previousIndex][1]
-            if sumPeak < diffrence:
-                dataset[previousIndex][1] += sumPeak
-                sumPeak -= sumPeak
-            else :
-                dataset[previousIndex][1] += diffrence
-                sumPeak -= diffrence
-            previousIndex -= 1
-    
-    plot(get_X(0, 1, dataset), get_Y(1, dataset), range(0, 25, 2), range(0, 3001, 500), 2500) # plot datapoints
-    y = [i[1] for i in dataset] # get all the Y 
-    print(f'total area: {simps(y, dx=1)}')
+    previousIndex = indexes[0] - 1 # get the index before the begining of the peak
+    while sumPeak > 0 and previousIndex != 0: # loops till sumPeak is 0
+        maxPrevious = dataset[previousIndex + 1][1] * (1 - curveRate)
+        diffrence = maxPrevious - dataset[previousIndex][1]
+        if sumPeak < diffrence:
+            dataset[previousIndex][1] += sumPeak
+            sumPeak -= sumPeak
+        else :
+            dataset[previousIndex][1] += diffrence
+            sumPeak -= diffrence
+        previousIndex -= 1
+
+plot(get_X(0, 1, dataset), get_Y(1, dataset), range(0, 25, 2), range(0, 6001, 500), 2500) # plot datapoints
+y = [i[1] for i in dataset] # get all the Y 
+print(f'total area: {simps(y, dx=1)}')
